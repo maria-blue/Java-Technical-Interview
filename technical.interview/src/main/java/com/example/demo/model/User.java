c3c15516e82549d381ec2b38e08555cf3604bc02
Maria Galani <maria-blue@users.noreply.github.com>
2025-08-26 17:18:59 +0200

    Small fix

--- technical.interview/src/main/java/com/example/demo/model/User.java
+++ technical.interview/src/main/java/com/example/demo/model/User.java
@@ -1,9 +1,6 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +8,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "app_user")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
